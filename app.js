c30e19152e8ddfbb2a17123e8f872617aef712cf
Gabriela Donato <gabi.donato726@gmail.com>
2025-11-09 18:18:47 -0500

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Welcome to the secret game!");
+let numeroSecreto = 5;
+let chute = prompt("Choose a number between 1 and 10:");
+
+if (numeroSecreto == chute) {
+   console.log("You guessed it right (5)!");
+}
