6e61602b7c79da875307a3cb997268162d67a4c5
Gabriela Donato <gabi.donato726@gmail.com>
2025-11-09 18:31:53 -0500

    else e comentarios

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 alert("Welcome to the secret game!");
 let numeroSecreto = 5;
+console.log("The secret number is: " + numeroSecreto);
 let chute = prompt("Choose a number between 1 and 10:");
 
 if (numeroSecreto == chute) {
-   console.log("You guessed it right (5)!");
+   alert("Congratulations! You've guessed the secret number!");
+}
+
+else {
+   alert("Wrong guess, try again!");
 }
